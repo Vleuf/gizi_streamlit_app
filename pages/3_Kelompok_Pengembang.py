2bfc15baeabfb25c4e30318563057c0a8e9b261d
Vleuf <ohkelasonline@gmail.com>
2025-05-13 13:27:21 +0700

    Update 3_Kelompok_Pengembang.py

--- pages/3_Kelompok_Pengembang.py
+++ pages/3_Kelompok_Pengembang.py
@@ -5,3 +5,12 @@ if st.button("Mulai Proses"):
     with st.spinner("Sedang diproses..."):
         time.sleep(2)
     st.success("Selesai!")
+
+
+import streamlit as st
+
+st.title("🎉 Ucapan Selamat!")
+
+if st.button("Tekan untuk Merayakan"):
+    st.success("Selamat! Kamu berhasil menyelesaikan tugas.")
+    st.snow()  # Efek konfeti akan muncul
